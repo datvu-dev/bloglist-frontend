3d1effa5a9788abe77c2028efb905b404b1b0843
Dat Vu <dothanhdat.vu@gmail.com>
2019-12-09 21:09:14 +1100

    Create a form to add a new blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,179 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import loginService from './services/login'
+import blogService from './services/blogs'
+import AddForm from './components/AddForm'
+const baseUrl = '/api/login'
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([]) 
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('') 
+  const [url, setUrl] = useState('') 
+  const [likes, setLikes] = useState('') 
+  const [ message, setMessage ] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService
+      .getAll().then(initialBlogs => {
+        setBlogs(initialBlogs)
+      })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      console.log(loggedUserJSON)
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.localStorage.clear()
+
+    setUser(null)
+  }
+
+  const handleAdd = async (event) => {
+    event.preventDefault()
+
+    try {
+      blogService.create(
+        {
+          title: title,
+          author: author,
+          url: url,
+          likes: likes
+        }
+      ).then(response => {
+        setBlogs(blogs.concat(response))
+
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+        setLikes('')
+
+        setMessage(
+          {content: `Added ${title.trim()}`, type: 'success'}
+        )
+
+        setTimeout(() => {
+          setMessage(null)
+        }, 4000)
+      })
+
+      
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+  }
+
+  const loginForm = () => (
+    <div>
+      <header className="App-header">
+        <h1>Login to application</h1>
+      </header>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>    
+    </div>  
+  )
+
+  const Blogs = () => {
+    const blogRows = () => blogs.map(blog =>
+      <p>{blog.title} ({blog.author})</p>
+    )
+
+    return (
+      <div>
+        {blogRows()}
+      </div>  
+    )
+  }
+
+  const blogList = () => {
+    return(
+      <div>
+        <header className="App-header">
+          <h1>Blogs</h1>
+        </header>
+        <div>
+          <p>{user.name} logged in <button type="submit" onClick={handleLogout}>logout</button></p>
+        </div>
+        <AddForm 
+          handleAdd={handleAdd} 
+          title={title} setTitle={setTitle}
+          author={author} setAuthor={setAuthor}
+          url={url} setUrl={setUrl}
+          likes={likes} setLikes={setLikes}
+        />
+        {Blogs()}
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user === null ?
+        loginForm() :
+        blogList()
+      }
     </div>
   );
 }
---
+++ src/components/AddForm.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+
+const AddForm = (props) => {
+    return (
+      <div>
+        <h2>Add a new blog</h2>
+        <form onSubmit={props.handleAdd}>
+          <div>
+            Title
+              <input
+              type="text"
+              value={props.title}
+              name="Title"
+              onChange={({ target }) => props.setTitle(target.value)}
+            />
+          </div>
+          <div>
+            Author
+              <input
+              type="text"
+              value={props.author}
+              name="Author"
+              onChange={({ target }) => props.setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            Url
+              <input
+              type="text"
+              value={props.url}
+              name="Url"
+              onChange={({ target }) => props.setUrl(target.value)}
+            />
+          </div><div>
+            Likes
+              <input
+              type="number"
+              value={props.likes}
+              name="Likes"
+              onChange={({ target }) => props.setLikes(target.value)}
+            />
+          </div>
+          <button type="submit">Add Blog</button>
+        </form>    
+      </div>  
+    )
+}
+
+export default AddForm
